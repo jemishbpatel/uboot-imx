2eeedb767745a99da34fe6d44edf751fe4d57888
Jemish Patel <jemish@liveu.tv>
2018-11-26 12:18:28 +0530

    Backport: feature: add ddr micron part
    Author: Hitesh Viradiya <Hitesh@liveu.tv>
    Date:   Thu Dec 3 14:48:29 2015 +0530
    
        feature: add ddr micron part
    
        This contais 2GB DDR3 primary changes done for Micron part# MT41K256M16
    
        Change-Id: I71c992eb9ad908fe1b8581d5d83712d44ff393be
        Signed-off-by: Hitesh Viradiya <Hitesh@liveu.tv>

--- board/liveuboard/spl.c
+++ board/liveuboard/spl.c
@@ -65,36 +65,6 @@ static struct mx6dq_iomux_ddr_regs mx6dq_ddr_ioregs = {
 	.dram_dqm7 = IMX6DQ_DRIVE_STRENGTH,
 };
 
-/* configure MX6QP mmdc DDR io registers */
-static struct mx6dq_iomux_ddr_regs mx6qp_ddr_ioregs = {
-	.dram_sdclk_0 = IMX6QP_DRIVE_STRENGTH,
-	.dram_sdclk_1 = IMX6QP_DRIVE_STRENGTH,
-	.dram_cas = IMX6QP_DRIVE_STRENGTH,
-	.dram_ras = IMX6QP_DRIVE_STRENGTH,
-	.dram_reset = IMX6QP_DRIVE_STRENGTH,
-	.dram_sdcke0 = IMX6QP_DRIVE_STRENGTH,
-	.dram_sdcke1 = IMX6QP_DRIVE_STRENGTH,
-	.dram_sdba2 = 0x00000000,
-	.dram_sdodt0 = IMX6QP_DRIVE_STRENGTH,
-	.dram_sdodt1 = IMX6QP_DRIVE_STRENGTH,
-	.dram_sdqs0 = IMX6QP_DRIVE_STRENGTH,
-	.dram_sdqs1 = IMX6QP_DRIVE_STRENGTH,
-	.dram_sdqs2 = IMX6QP_DRIVE_STRENGTH,
-	.dram_sdqs3 = IMX6QP_DRIVE_STRENGTH,
-	.dram_sdqs4 = IMX6QP_DRIVE_STRENGTH,
-	.dram_sdqs5 = IMX6QP_DRIVE_STRENGTH,
-	.dram_sdqs6 = IMX6QP_DRIVE_STRENGTH,
-	.dram_sdqs7 = IMX6QP_DRIVE_STRENGTH,
-	.dram_dqm0 = IMX6QP_DRIVE_STRENGTH,
-	.dram_dqm1 = IMX6QP_DRIVE_STRENGTH,
-	.dram_dqm2 = IMX6QP_DRIVE_STRENGTH,
-	.dram_dqm3 = IMX6QP_DRIVE_STRENGTH,
-	.dram_dqm4 = IMX6QP_DRIVE_STRENGTH,
-	.dram_dqm5 = IMX6QP_DRIVE_STRENGTH,
-	.dram_dqm6 = IMX6QP_DRIVE_STRENGTH,
-	.dram_dqm7 = IMX6QP_DRIVE_STRENGTH,
-};
-
 /* configure MX6Q/DUAL mmdc GRP io registers */
 static struct mx6dq_iomux_grp_regs mx6dq_grp_ioregs = {
 	.grp_ddr_type = 0x000c0000,
@@ -113,74 +83,8 @@ static struct mx6dq_iomux_grp_regs mx6dq_grp_ioregs = {
 	.grp_b7ds = IMX6DQ_DRIVE_STRENGTH,
 };
 
-/* configure MX6QP mmdc GRP io registers */
-static struct mx6dq_iomux_grp_regs mx6qp_grp_ioregs = {
-	.grp_ddr_type = 0x000c0000,
-	.grp_ddrmode_ctl = 0x00020000,
-	.grp_ddrpke = 0x00000000,
-	.grp_addds = IMX6QP_DRIVE_STRENGTH,
-	.grp_ctlds = IMX6QP_DRIVE_STRENGTH,
-	.grp_ddrmode = 0x00020000,
-	.grp_b0ds = IMX6QP_DRIVE_STRENGTH,
-	.grp_b1ds = IMX6QP_DRIVE_STRENGTH,
-	.grp_b2ds = IMX6QP_DRIVE_STRENGTH,
-	.grp_b3ds = IMX6QP_DRIVE_STRENGTH,
-	.grp_b4ds = IMX6QP_DRIVE_STRENGTH,
-	.grp_b5ds = IMX6QP_DRIVE_STRENGTH,
-	.grp_b6ds = IMX6QP_DRIVE_STRENGTH,
-	.grp_b7ds = IMX6QP_DRIVE_STRENGTH,
-};
-
-/* configure MX6SOLO/DUALLITE mmdc DDR io registers */
-struct mx6sdl_iomux_ddr_regs mx6sdl_ddr_ioregs = {
-	.dram_sdclk_0 = IMX6SDL_DRIVE_STRENGTH,
-	.dram_sdclk_1 = IMX6SDL_DRIVE_STRENGTH,
-	.dram_cas = IMX6SDL_DRIVE_STRENGTH,
-	.dram_ras = IMX6SDL_DRIVE_STRENGTH,
-	.dram_reset = IMX6SDL_DRIVE_STRENGTH,
-	.dram_sdcke0 = IMX6SDL_DRIVE_STRENGTH,
-	.dram_sdcke1 = IMX6SDL_DRIVE_STRENGTH,
-	.dram_sdba2 = 0x00000000,
-	.dram_sdodt0 = IMX6SDL_DRIVE_STRENGTH,
-	.dram_sdodt1 = IMX6SDL_DRIVE_STRENGTH,
-	.dram_sdqs0 = IMX6SDL_DRIVE_STRENGTH,
-	.dram_sdqs1 = IMX6SDL_DRIVE_STRENGTH,
-	.dram_sdqs2 = IMX6SDL_DRIVE_STRENGTH,
-	.dram_sdqs3 = IMX6SDL_DRIVE_STRENGTH,
-	.dram_sdqs4 = IMX6SDL_DRIVE_STRENGTH,
-	.dram_sdqs5 = IMX6SDL_DRIVE_STRENGTH,
-	.dram_sdqs6 = IMX6SDL_DRIVE_STRENGTH,
-	.dram_sdqs7 = IMX6SDL_DRIVE_STRENGTH,
-	.dram_dqm0 = IMX6SDL_DRIVE_STRENGTH,
-	.dram_dqm1 = IMX6SDL_DRIVE_STRENGTH,
-	.dram_dqm2 = IMX6SDL_DRIVE_STRENGTH,
-	.dram_dqm3 = IMX6SDL_DRIVE_STRENGTH,
-	.dram_dqm4 = IMX6SDL_DRIVE_STRENGTH,
-	.dram_dqm5 = IMX6SDL_DRIVE_STRENGTH,
-	.dram_dqm6 = IMX6SDL_DRIVE_STRENGTH,
-	.dram_dqm7 = IMX6SDL_DRIVE_STRENGTH,
-};
-
-/* configure MX6SOLO/DUALLITE mmdc GRP io registers */
-struct mx6sdl_iomux_grp_regs mx6sdl_grp_ioregs = {
-	.grp_ddr_type = 0x000c0000,
-	.grp_ddrmode_ctl = 0x00020000,
-	.grp_ddrpke = 0x00000000,
-	.grp_addds = IMX6SDL_DRIVE_STRENGTH,
-	.grp_ctlds = IMX6SDL_DRIVE_STRENGTH,
-	.grp_ddrmode = 0x00020000,
-	.grp_b0ds = IMX6SDL_DRIVE_STRENGTH,
-	.grp_b1ds = IMX6SDL_DRIVE_STRENGTH,
-	.grp_b2ds = IMX6SDL_DRIVE_STRENGTH,
-	.grp_b3ds = IMX6SDL_DRIVE_STRENGTH,
-	.grp_b4ds = IMX6SDL_DRIVE_STRENGTH,
-	.grp_b5ds = IMX6SDL_DRIVE_STRENGTH,
-	.grp_b6ds = IMX6SDL_DRIVE_STRENGTH,
-	.grp_b7ds = IMX6SDL_DRIVE_STRENGTH,
-};
-
-/* H5T04G63AFR-PB */
-static struct mx6_ddr3_cfg h5t04g63afr = {
+/* MT41K256M16HA-125 (4Gb density) */
+static struct mx6_ddr3_cfg mt41k256m16ha_125 = {
 	.mem_speed = 1600,
 	.density = 4,
 	.width = 16,
@@ -191,102 +95,32 @@ static struct mx6_ddr3_cfg h5t04g63afr = {
 	.trcd = 1375,
 	.trcmin = 4875,
 	.trasmin = 3500,
-};
+ };
 
-/* H5TQ2G63DFR-H9 */
-static struct mx6_ddr3_cfg h5tq2g63dfr = {
-	.mem_speed = 1333,
-	.density = 2,
-	.width = 16,
-	.banks = 8,
-	.rowaddr = 14,
-	.coladdr = 10,
-	.pagesz = 2,
-	.trcd = 1350,
-	.trcmin = 4950,
-	.trasmin = 3600,
-};
-
-static struct mx6_mmdc_calibration mx6q_2g_mmdc_calib = {
-	.p0_mpwldectrl0 = 0x001f001f,
-	.p0_mpwldectrl1 = 0x001f001f,
-	.p1_mpwldectrl0 = 0x001f001f,
-	.p1_mpwldectrl1 = 0x001f001f,
-	.p0_mpdgctrl0 = 0x4301030d,
-	.p0_mpdgctrl1 = 0x03020277,
-	.p1_mpdgctrl0 = 0x4300030a,
-	.p1_mpdgctrl1 = 0x02780248,
-	.p0_mprddlctl = 0x4536393b,
-	.p1_mprddlctl = 0x36353441,
-	.p0_mpwrdlctl = 0x41414743,
-	.p1_mpwrdlctl = 0x462f453f,
+/* This is taken from some other board having same miron ddr chip part
+   number */
+static struct mx6_mmdc_calibration mx6q_4g_mmdc_calib = {
+	/* write leveling calibration determine */
+	.p0_mpwldectrl0 = 0X00220021,
+	.p0_mpwldectrl1 = 0X00200030,
+	.p1_mpwldectrl0 = 0X002D0027,
+	.p1_mpwldectrl1 = 0X00150026,
+	/* Read DQS Gating calibration */
+	.p0_mpdgctrl0 = 0x43330342,
+	.p0_mpdgctrl1 = 0x0339034A,
+	.p1_mpdgctrl0 = 0x032F0325,
+	.p1_mpdgctrl1 = 0x032F022E,
+	/* Read Calibration: DQS delay relative to DQ read access */
+	.p0_mprddlctl = 0X3A2E3437,
+	.p1_mprddlctl = 0X35312F3F,
+	/* Write Calibration: DQ/DM delay relative to DQS write access */
+	.p0_mpwrdlctl = 0X33363B37,
+	.p1_mpwrdlctl = 0X40304239,
 };
 
 /* DDR 64bit 2GB */
 static struct mx6_ddr_sysinfo mem_q = {
 	.dsize		= 2,
-	.cs1_mirror	= 0,
-	/* config for full 4GB range so that get_mem_size() works */
-	.cs_density	= 32,
-	.ncs		= 1,
-	.bi_on		= 1,
-	.rtt_nom	= 1,
-	.rtt_wr		= 0,
-	.ralat		= 5,
-	.walat		= 0,
-	.mif3_mode	= 3,
-	.rst_to_cke	= 0x23,
-	.sde_to_rst	= 0x10,
-	.refsel = 1,	/* Refresh cycles at 32KHz */
-	.refr = 3,	/* 4 refresh commands per refresh cycle */
-};
-
-static struct mx6_mmdc_calibration mx6dl_1g_mmdc_calib = {
-	.p0_mpwldectrl0 = 0x001f001f,
-	.p0_mpwldectrl1 = 0x001f001f,
-	.p1_mpwldectrl0 = 0x001f001f,
-	.p1_mpwldectrl1 = 0x001f001f,
-	.p0_mpdgctrl0 = 0x420e020e,
-	.p0_mpdgctrl1 = 0x02000200,
-	.p1_mpdgctrl0 = 0x42020202,
-	.p1_mpdgctrl1 = 0x01720172,
-	.p0_mprddlctl = 0x494c4f4c,
-	.p1_mprddlctl = 0x4a4c4c49,
-	.p0_mpwrdlctl = 0x3f3f3133,
-	.p1_mpwrdlctl = 0x39373f2e,
-};
-
-static struct mx6_mmdc_calibration mx6s_512m_mmdc_calib = {
-	.p0_mpwldectrl0 = 0x0040003c,
-	.p0_mpwldectrl1 = 0x0032003e,
-	.p0_mpdgctrl0 = 0x42350231,
-	.p0_mpdgctrl1 = 0x021a0218,
-	.p0_mprddlctl = 0x4b4b4e49,
-	.p0_mpwrdlctl = 0x3f3f3035,
-};
-
-/* DDR 64bit 1GB */
-static struct mx6_ddr_sysinfo mem_dl = {
-	.dsize		= 2,
-	.cs1_mirror	= 0,
-	/* config for full 4GB range so that get_mem_size() works */
-	.cs_density	= 32,
-	.ncs		= 1,
-	.bi_on		= 1,
-	.rtt_nom	= 1,
-	.rtt_wr		= 0,
-	.ralat		= 5,
-	.walat		= 0,
-	.mif3_mode	= 3,
-	.rst_to_cke	= 0x23,
-	.sde_to_rst	= 0x10,
-	.refsel = 1,	/* Refresh cycles at 32KHz */
-	.refr = 3,	/* 4 refresh commands per refresh cycle */
-};
-
-/* DDR 32bit 512MB */
-static struct mx6_ddr_sysinfo mem_s = {
-	.dsize		= 1,
 	.cs1_mirror	= 0,
 	/* config for full 4GB range so that get_mem_size() works */
 	.cs_density	= 32,
@@ -316,90 +150,11 @@ static void ccgr_init(void)
 	writel(0x000003FF, &ccm->CCGR6);
 }
 
-static void spl_dram_init_imx6qp_lpddr3(void)
-{
-	/* MMDC0_MDSCR set the Configuration request bit during MMDC set up */
-	writel(0x00008000, MMDC_P0_BASE_ADDR + 0x01c);
-	/* Calibrations - ZQ */
-	writel(0xa1390003, MMDC_P0_BASE_ADDR + 0x800);
-	/* write leveling */
-	writel(0x00060004, MMDC_P0_BASE_ADDR + 0x80c);
-	writel(0x000B0004, MMDC_P0_BASE_ADDR + 0x810);
-	writel(0x00000004, MMDC_P1_BASE_ADDR + 0x80c);
-	writel(0x00000000, MMDC_P1_BASE_ADDR + 0x810);
-	/*
-	 * DQS gating, read delay, write delay calibration values
-	 * based on calibration compare of 0x00ffff00
-	 */
-	writel(0x03040314, MMDC_P0_BASE_ADDR + 0x83c);
-	writel(0x03080300, MMDC_P0_BASE_ADDR + 0x840);
-	writel(0x03000310, MMDC_P1_BASE_ADDR + 0x83c);
-	writel(0x0268023C, MMDC_P1_BASE_ADDR + 0x840);
-	writel(0x4034363A, MMDC_P0_BASE_ADDR + 0x848);
-	writel(0x36302C3C, MMDC_P1_BASE_ADDR + 0x848);
-	writel(0x3E3E4046, MMDC_P0_BASE_ADDR + 0x850);
-	writel(0x483A4844, MMDC_P1_BASE_ADDR + 0x850);
-	writel(0x33333333, MMDC_P0_BASE_ADDR + 0x81c);
-	writel(0x33333333, MMDC_P0_BASE_ADDR + 0x820);
-	writel(0x33333333, MMDC_P0_BASE_ADDR + 0x824);
-	writel(0x33333333, MMDC_P0_BASE_ADDR + 0x828);
-	writel(0x33333333, MMDC_P1_BASE_ADDR + 0x81c);
-	writel(0x33333333, MMDC_P1_BASE_ADDR + 0x820);
-	writel(0x33333333, MMDC_P1_BASE_ADDR + 0x824);
-	writel(0x33333333, MMDC_P1_BASE_ADDR + 0x828);
-	writel(0x24912489, MMDC_P0_BASE_ADDR + 0x8c0);
-	writel(0x24914452, MMDC_P1_BASE_ADDR + 0x8c0);
-	writel(0x00000800, MMDC_P0_BASE_ADDR + 0x8b8);
-	writel(0x00000800, MMDC_P1_BASE_ADDR + 0x8b8);
-	/* MMDC init: in DDR3, 64-bit mode, only MMDC0 is initiated */
-	writel(0x00020036, MMDC_P0_BASE_ADDR + 0x004);
-	writel(0x09444040, MMDC_P0_BASE_ADDR + 0x008);
-	writel(0x898E79A4, MMDC_P0_BASE_ADDR + 0x00c);
-	writel(0xDB538F64, MMDC_P0_BASE_ADDR + 0x010);
-	writel(0x01FF00DD, MMDC_P0_BASE_ADDR + 0x014);
-	writel(0x00011740, MMDC_P0_BASE_ADDR + 0x018);
-	writel(0x00008000, MMDC_P0_BASE_ADDR + 0x01c);
-	writel(0x000026D2, MMDC_P0_BASE_ADDR + 0x02c);
-	writel(0x008E1023, MMDC_P0_BASE_ADDR + 0x030);
-	writel(0x00000047, MMDC_P0_BASE_ADDR + 0x040);
-	writel(0x14420000, MMDC_P0_BASE_ADDR + 0x400);
-	writel(0x841A0000, MMDC_P0_BASE_ADDR + 0x000);
-	writel(0x00400c58, MMDC_P0_BASE_ADDR + 0x890);
-	/* add NOC DDR configuration */
-	writel(0x00000000, NOC_DDR_BASE_ADDR + 0x008);
-	writel(0x2871C39B, NOC_DDR_BASE_ADDR + 0x00c);
-	writel(0x000005B4, NOC_DDR_BASE_ADDR + 0x038);
-	writel(0x00000040, NOC_DDR_BASE_ADDR + 0x014);
-	writel(0x00000020, NOC_DDR_BASE_ADDR + 0x028);
-	writel(0x00000020, NOC_DDR_BASE_ADDR + 0x02c);
-	writel(0x02088032, MMDC_P0_BASE_ADDR + 0x01c);
-	writel(0x00008033, MMDC_P0_BASE_ADDR + 0x01c);
-	writel(0x00048031, MMDC_P0_BASE_ADDR + 0x01c);
-	writel(0x19308030, MMDC_P0_BASE_ADDR + 0x01c);
-	writel(0x04008040, MMDC_P0_BASE_ADDR + 0x01c);
-	writel(0x00007800, MMDC_P0_BASE_ADDR + 0x020);
-	writel(0x00022227, MMDC_P0_BASE_ADDR + 0x818);
-	writel(0x00022227, MMDC_P1_BASE_ADDR + 0x818);
-	writel(0x00025576, MMDC_P0_BASE_ADDR + 0x004);
-	writel(0x00011006, MMDC_P0_BASE_ADDR + 0x404);
-	writel(0x00000000, MMDC_P0_BASE_ADDR + 0x01c);
-}
-
 static void spl_dram_init(void)
 {
-	if (is_mx6dqp()) {
-		mx6dq_dram_iocfg(64, &mx6qp_ddr_ioregs, &mx6qp_grp_ioregs);
-		spl_dram_init_imx6qp_lpddr3();
-	} else if (is_cpu_type(MXC_CPU_MX6SOLO)) {
-		mx6sdl_dram_iocfg(32, &mx6sdl_ddr_ioregs, &mx6sdl_grp_ioregs);
-		mx6_dram_cfg(&mem_s, &mx6s_512m_mmdc_calib, &h5tq2g63dfr);
-	} else if (is_cpu_type(MXC_CPU_MX6DL)) {
-		mx6sdl_dram_iocfg(64, &mx6sdl_ddr_ioregs, &mx6sdl_grp_ioregs);
-		mx6_dram_cfg(&mem_dl, &mx6dl_1g_mmdc_calib, &h5tq2g63dfr);
-	} else if (is_cpu_type(MXC_CPU_MX6Q)) {
-		mx6dq_dram_iocfg(64, &mx6dq_ddr_ioregs, &mx6dq_grp_ioregs);
-		mx6_dram_cfg(&mem_q, &mx6q_2g_mmdc_calib, &h5t04g63afr);
-	}
+	/* No of Data line : width = 64 */
+	mx6dq_dram_iocfg(64, &mx6dq_ddr_ioregs, &mx6dq_grp_ioregs);
+	mx6_dram_cfg(&mem_q, &mx6q_4g_mmdc_calib, &mt41k256m16ha_125);
 
 	udelay(100);
 }
